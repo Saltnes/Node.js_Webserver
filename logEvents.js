1f6cfb38101b85495e983e786d7d476c1ac51b8a
saltnes <robinbeg@gmail.com>
2023-01-20 09:49:13 +0100

    Webserver. Can serve images

--- logEvents.js
+++ logEvents.js
@@ -7,11 +7,15 @@ const path = require('path');
 
 const logEvents = async (message) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
-    const logItem = `${dateTime}\t${uuid()}\t${message}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
     try{
+        if (!fs.existsSync(path.join(__dirname, 'logs'))){
+            await fsPromises.mkdir(path.join(__dirname, 'logs'))
+        }
+        // testings
         await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem);
     } catch (err) {
-        console.err(err)
+        console.log(err)
     }
 }
 
